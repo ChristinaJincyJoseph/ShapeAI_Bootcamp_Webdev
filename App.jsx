fff9c06c08c4955c3f04949f94e71998373cde94
ChristinaJincyJoseph <84991944+ChristinaJincyJoseph@users.noreply.github.com>
2021-06-07 01:55:06 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note1 from "./Note1"
+
+
+function App(){
+  return(<div>
+            <Header />
+            <Footer />
+            <Note1 />
+            <Note1 />
+            <Note1 />
+
+         </div>
+    );
+
+ }
+ export default App;
---
+++ Note1.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Note1(){
+  return(
+     <div className="note">
+        <h1>Javascript and React.js</h1>
+        
+        <p>An amazing bootcamp to learn new in-demand technologies with hands on projects along with certificates from ShapeAI, MLSA and AWS.</p>
+        <p>Here they teach everything from scratch which includes Javascript,React.js,HTML.</p>
+
+     </div>
+
+    );
+
+ }
+ export default Note1;
